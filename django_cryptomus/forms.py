1d054d908448ee856ec4b1ac7ba3b2e05f3191ce
root <adamak.tnh@gmail.com>
2024-05-31 15:22:09 +0330

    delete success and callback field in clean code form

--- django_cryptomus/forms.py
+++ django_cryptomus/forms.py
@@ -7,3 +7,15 @@ class CryptoPaymentForm(forms.Form):
     order_id = forms.CharField(max_length=100, required=False)
     # Define an optional JSON field to accept additional data
     additional_data = forms.JSONField(required=False)
+
+    def clean(self):
+        
+        cleaned_data = super().clean()
+        additional_data = cleaned_data.get("additional_data", {})
+        if additional_data:
+            for field in ["url_success", "url_callback"]:
+                if field in additional_data:
+                    additional_data.pop(field)
+            cleaned_data["additional_data"] = additional_data     
+
+        return cleaned_data
